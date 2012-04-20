28debab577c416687ca0be8be6c99c635367de15
Dave Arneson <urvaius@gmail.com>
2012-04-20 16:24:20 -0700

    formatting

--- CreapyWeather/src/com/arne5/creapyweather/WeatherXMLParsing.java
+++ CreapyWeather/src/com/arne5/creapyweather/WeatherXMLParsing.java
@@ -17,74 +17,67 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class WeatherXMLParsing extends Activity implements OnClickListener {
-	
+
 	static final String baseURL = "http://www.google.com/ig/api?weather=";
-	 TextView tv;
-	 EditText city, state;
-	 TextView tvcon;
-	 
-	
-   /** Called when the activity is first created. */
-   @Override
-   public void onCreate(Bundle icicle) {
-           super.onCreate(icicle);
-           //load sound from creapysound class
-           CreapySound.loadSound(this);
-           setContentView(R.layout.weather);
-           Button b = (Button)findViewById(R.id.bWeather);
-           tv = (TextView)findViewById(R.id.tvWeather);
-           city = (EditText)findViewById(R.id.etCity);
-           state = (EditText)findViewById(R.id.etState);
-           b.setOnClickListener(this);
-           tvcon = (TextView)findViewById(R.id.tvCon);
-           
-  
-   }
+	TextView tv;
+	EditText city, state;
+	TextView tvcon;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		// load sound from creapysound class
+		CreapySound.loadSound(this);
+		setContentView(R.layout.weather);
+		Button b = (Button) findViewById(R.id.bWeather);
+		tv = (TextView) findViewById(R.id.tvWeather);
+		city = (EditText) findViewById(R.id.etCity);
+		state = (EditText) findViewById(R.id.etState);
+		b.setOnClickListener(this);
+		tvcon = (TextView) findViewById(R.id.tvCon);
+
+	}
 
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		String c = city.getText().toString();
 		String s = state.getText().toString();
-		String sun ="Mostly Sunny";
-		
+		String sun = "Mostly Sunny";
+
 		StringBuilder URL = new StringBuilder(baseURL);
 		URL.append(c + "," + s);
 		String fullUrl = URL.toString();
-		try{
+		try {
 			URL website = new URL(fullUrl);
-			//getting xmlreader to parse data
+			// getting xmlreader to parse data
 			SAXParserFactory spf = SAXParserFactory.newInstance();
 			SAXParser sp = spf.newSAXParser();
 			XMLReader xr = sp.getXMLReader();
-			
+
 			WeatherXMLHandler conditionWork = new WeatherXMLHandler();
 			xr.setContentHandler(conditionWork);
 			xr.parse(new InputSource(website.openStream()));
-			String conditioninfo= conditionWork.conditionInfo();
-			
+			String conditioninfo = conditionWork.conditionInfo();
+
 			WeatherXMLHandler doingWork = new WeatherXMLHandler();
 			xr.setContentHandler(doingWork);
 			xr.parse(new InputSource(website.openStream()));
 			String information = doingWork.getInformation();
-			
-			
+
 			tvcon.setText(conditioninfo);
-			//set some variables to check and play sound
 			
-			
-			
+
 			// get the tvcon text and see if it equals sun string.
-			if(tvcon.getText().toString().equals(sun))
+			if (tvcon.getText().toString().equals(sun))
 				CreapySound.playCloudy();
-			
-			
-			
+
 			tv.setText(information);
-			//CreapySound.playCloudy();
-			
-		}catch (Exception e){
+			// CreapySound.playCloudy();
+
+		} catch (Exception e) {
 			tv.setText("error setting weather");
 		}
-		
+
 	}
 }
